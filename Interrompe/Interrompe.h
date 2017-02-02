2d25b2d5f02afd73d96c87952b83e7cabece304f
Daniel Leite Ribeiro <daniel.leite.r@hotmail.com>
2017-02-01 23:51:37 -0200

    Adicionadas funções desabilitaInt e modificaInt

--- Interrompe/Interrompe.h
+++ Interrompe/Interrompe.h
@@ -9,27 +9,46 @@
 #ifndef Interrompe_h
 #define Interrompe_h
 
+#include "Arduino.h"
+
 uint8_t conf_borda_sub[3], conf_borda_desc[3];
 uint8_t PORTB_anterior, PORTC_anterior, PORTD_anterior;
 
 bool limpar_flag = false;
 
-typedef void (*tipo_isr)();
-tipo_isr isr_personalizada_B[8], isr_personalizada_C[7], isr_personalizada_D[8];
+typedef void (*voidFuncPtr)();
+voidFuncPtr isr_personalizada_B[8], isr_personalizada_C[7], isr_personalizada_D[8];
 
-void configInterromp(const byte pino, tipo_isr isr_ptr, const byte tipo)
+bool habilitaInt(const uint8_t pino, voidFuncPtr isr_ptr, const byte tipo)
 {
-  if(digitalPinToInterrupt(pino) != NOT_AN_INTERRUPT)    // O ATmega328P possui registradores de interrupção separados para os pinos PD2 e
-  {                                                      // PD3, que apresentam ISRs próprias e têm prioridade em relação aos outros pinos
+  if(digitalPinToPCICR(pino) == NULL) // Pino inválido
+    return false;
+  
+ if(digitalPinToInterrupt(pino) != NOT_AN_INTERRUPT)    // O ATmega328P possui registradores de interrupção separados para os pinos PD2 e
+  {                                                     // PD3, que apresentam ISRs próprias e têm prioridade em relação aos outros pinos
     if(limpar_flag)
       bitSet(EIFR, digitalPinToInterrupt(pino));
       
     attachInterrupt(digitalPinToInterrupt(pino), isr_ptr, tipo);    // Para esses pinos já existe função de alto nível
-    return;
+    return true;
   }
 
-  uint8_t bit_grupo = digitalPinToPCICRbit(pino);
+ uint8_t bit_grupo = digitalPinToPCICRbit(pino);
   uint8_t bit_mascara = digitalPinToPCMSKbit(pino);
+
+  switch(bit_grupo)
+  {
+  case 0:
+    isr_personalizada_B[bit_mascara] = isr_ptr;
+    break;
+
+  case 1:
+    isr_personalizada_C[bit_mascara] = isr_ptr;
+    break;
+
+  case 2:
+    isr_personalizada_D[bit_mascara] = isr_ptr;
+  }
   
   if(limpar_flag)
     bitSet(PCIFR, bit_grupo);
@@ -42,6 +61,73 @@ void configInterromp(const byte pino, tipo_isr isr_ptr, const byte tipo)
   
   bitSet(PCICR, bit_grupo);   // Habilita o grupo de pinos (são três)
   bitSet(*digitalPinToPCMSK(pino), bit_mascara);  // Habilita o pino dentro do grupo
+
+  return true;
+}
+
+bool desabilitaInt(const uint8_t pino)
+{
+  if(digitalPinToPCICR(pino) == NULL) // Pino inválido
+    return false;
+  
+ if(digitalPinToInterrupt(pino) != NOT_AN_INTERRUPT)
+  {
+    detachInterrupt(digitalPinToInterrupt(pino));
+    return true;
+  }
+
+  uint8_t bit_grupo = digitalPinToPCICRbit(pino);
+  uint8_t bit_mascara = digitalPinToPCMSKbit(pino);
+
+  bitClear(conf_borda_sub[bit_grupo], bit_mascara);
+  bitClear(conf_borda_desc[bit_grupo], bit_mascara);
+  bitClear(*digitalPinToPCMSK(pino), bit_mascara);  // Desabilita o pino dentro do grupo
+
+  if(*digitalPinToPCMSK(pino) == 0) // Se não tiver sobrado nenhum pino nesse grupo,
+    bitClear(PCICR, bit_grupo);     // desabilita o grupo
+
+  return true;
+}
+
+bool modificaInt(const uint8_t pino, const uint8_t tipo)
+{
+  if(digitalPinToPCICR(pino) == NULL) // Pino inválido
+    return false;
+
+  uint8_t bit_grupo = digitalPinToPCICRbit(pino);
+  uint8_t bit_mascara = digitalPinToPCMSKbit(pino);
+
+  bitClear(conf_borda_sub[bit_grupo], bit_mascara);
+  bitClear(conf_borda_desc[bit_grupo], bit_mascara);
+
+  if(tipo == RISING || tipo == CHANGE)
+    bitSet(conf_borda_sub[bit_grupo], bit_mascara);
+
+  if(tipo == FALLING || tipo == CHANGE)
+    bitSet(conf_borda_desc[bit_grupo], bit_mascara);
+}
+
+bool modificaInt(const uint8_t pino, voidFuncPtr isr_ptr)
+{
+  if(digitalPinToPCICR(pino) == NULL) // Pino inválido
+    return false;
+
+  uint8_t bit_grupo = digitalPinToPCICRbit(pino);
+  uint8_t bit_mascara = digitalPinToPCMSKbit(pino);  
+  
+  switch(bit_grupo)
+  {
+  case 0:
+    isr_personalizada_B[bit_mascara] = isr_ptr;
+    break;
+
+  case 1:
+    isr_personalizada_C[bit_mascara] = isr_ptr;
+    break;
+
+  case 2:
+    isr_personalizada_D[bit_mascara] = isr_ptr;
+  }
 }
 
 void permitirInterrupcoesImediatas(bool permitir)
@@ -54,7 +140,7 @@ void permitirInterrupcoesImediatas(bool permitir)
 
 ISR(PCINT0_vect)
 {
-  uint8_t i, validos = (PORTB & ~PORTB_anterior & conf_borda_sub[0]) | (~PORTB & PORTB_anterior & conf_borda_desc[0]);
+  uint8_t validos = (PORTB & ~PORTB_anterior & conf_borda_sub[0]) | (~PORTB & PORTB_anterior & conf_borda_desc[0]);
   
   chamar(0x01,0);
   chamar(0x02,1);
@@ -72,7 +158,7 @@ ISR(PCINT0_vect)
 
 ISR(PCINT1_vect)
 {
-  uint8_t i, validos = (PORTC & ~PORTC_anterior & conf_borda_sub[1]) | (~PORTC & PORTC_anterior & conf_borda_desc[1]);
+  uint8_t validos = (PORTC & ~PORTC_anterior & conf_borda_sub[1]) | (~PORTC & PORTC_anterior & conf_borda_desc[1]);
 
   chamar(0x01,0);
   chamar(0x02,1);
@@ -89,12 +175,12 @@ ISR(PCINT1_vect)
 
 ISR(PCINT2_vect)
 {
-  uint8_t i, validos = (PORTD & ~PORTD_anterior & conf_borda_sub[2]) | (~PORTD & PORTD_anterior & conf_borda_desc[2]);
+  uint8_t validos = (PORTD & ~PORTD_anterior & conf_borda_sub[2]) | (~PORTD & PORTD_anterior & conf_borda_desc[2]);
   
   chamar(0x01,0);
   chamar(0x02,1);
-  chamar(0x04,2);
-  chamar(0x08,3);
+//chamar(0x04,2); // PD2 e PD3 têm suas próprias ISR
+//chamar(0x08,3);
   chamar(0x10,4);
   chamar(0x20,5);
   chamar(0x40,6);
